e10b1d0dccc6584b6fe26188f76d2bb2a6da371b
Hadi Nayebi <hadinayebi@gmail.com>
2022-01-07 18:03:55 -0500

    remove w arg from consensus/consensus_acc func add get_seq func

--- SeqEN2/utils/seq_tools.py
+++ SeqEN2/utils/seq_tools.py
@@ -7,8 +7,8 @@ __version__ = "0.0.1"
 from torch import diagonal, eye, fliplr, index_select, mode, tensor
 
 
-def consensus(output, ndx, w, device):
-    output_length = len(output)
+def consensus(output, ndx, device):
+    output_length, w = output.shape
     seq_length = output_length + w - 1
     filter_size = min(seq_length - ndx, ndx + 1)
     if filter_size > w:
@@ -20,14 +20,30 @@ def consensus(output, ndx, w, device):
     c_max = min(ndx, w - 1) + 1
     c_indices = tensor(range(c_min, c_max), device=device)
     sub_result = index_select(index_select(output, 0, r_indices), 1, c_indices)
-    return mode(diagonal(fliplr(fliplr(eye(filter_size, device=device).long()) * sub_result))).values.item()
+    val = mode(diagonal(fliplr(fliplr(eye(filter_size, device=device).long()) * sub_result)))
+    return val.values.item()
 
 
-def consensus_acc(seq, output, w, device):
+def get_seq(ndx, ndx_windows):
+    output_length, w = ndx_windows.shape
+    seq_length = output_length + w - 1
+    if ndx < output_length:
+        return ndx_windows[ndx][0]
+    elif ndx < seq_length:
+        return ndx_windows[-1][ndx - output_length + 1]
+    else:
+        raise IndexError(
+            f"index {ndx-output_length+1} is out of bounds for dimension 1 with size {w}"
+        )
+
+
+def consensus_acc(seq, output, device):
+    output_length, w = output.shape
+    seq_length = output_length + w - 1
     n = 0
     consensus_seq = []
-    for i, ndx in enumerate(seq):
-        consensus_seq.append(consensus(output, i, w, device=device))
-        if ndx.item() == consensus_seq[-1]:
+    for i in range(seq_length):
+        consensus_seq.append(consensus(output, i, device=device))
+        if get_seq(i, seq).item() == consensus_seq[-1]:
             n += 1
     return n / len(seq), consensus_seq
