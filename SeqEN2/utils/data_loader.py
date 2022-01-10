2e0abb37c51aa1df582f1a779de9133152e74a6c
Hadi Nayebi <hadinayebi@gmail.com>
2022-01-10 01:51:03 -0500

    DataLoader inherit from object define properties/setter adding docstrings return types

--- SeqEN2/utils/data_loader.py
+++ SeqEN2/utils/data_loader.py
@@ -4,7 +4,8 @@ import gzip
 import json
 from typing import Any
 
-import numpy as np
+from numpy import array, ndarray, unique
+from numpy.random import choice
 from pandas import DataFrame, concat, read_csv
 
 
@@ -12,7 +13,7 @@ class NumpyEncoder(json.JSONEncoder):
     """Enables JSON too encode numpy nd-arrays."""
 
     def default(self, obj) -> Any:
-        if isinstance(obj, np.ndarray):
+        if isinstance(obj, ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
@@ -70,45 +71,77 @@ def load_csv(filename) -> DataFrame:
         raise IOError("File (or list of files) format must be .csv or .csv.gz")
 
 
-class DataLoader:
-    def __init__(self):
-        self.train_data_files = None
-        self.test_data_files = None
-        self.train_data = None
-        self.test_data = None
-        self.test_items = None
+class DataLoader(object):
+    """DataLoader maintains train/test data for training/testing model."""
 
-    def set_train_data_files(self, train_data_files):
-        self.train_data_files = train_data_files
+    # TODO: DataLoader loads indexed protein data and creates sliding window on yield
 
-    def set_test_data_files(self, test_data_files):
-        self.test_data_files = test_data_files
+    def __init__(self) -> None:
+        self._train_data_files = None
+        self._test_data_files = None
+        self._train_data = None
+        self._test_data = None
+        self._test_items = None
 
-    def prepare_test_data(self):
-        if self.test_data is None:
-            self.test_data = load(self.test_data_files)
-            self.test_items = np.unique(
+    @property
+    def train_data_files(self) -> list:
+        return self._train_data_files
+
+    @train_data_files.setter
+    def train_data_files(self, train_data_files) -> None:
+        self._train_data_files = train_data_files
+
+    @property
+    def test_data_files(self) -> list:
+        return self._test_data_files
+
+    @test_data_files.setter
+    def test_data_files(self, test_data_files) -> None:
+        self._test_data_files = test_data_files
+
+    @property
+    def train_data(self) -> DataFrame:
+        return self._train_data
+
+    @property
+    def test_data(self) -> DataFrame:
+        return self._test_data
+
+    @property
+    def test_items(self) -> array:
+        return self._test_items
+
+    def prepare_test_data(self) -> None:
+        """Extract unique protein keys when loading test data for the first time."""
+        # TODO: will be removed
+        if self._test_data is None:
+            self._test_data = load_csv(self._test_data_files)
+            self._test_items = unique(
                 ["_".join(item.split("_")[:-1]) for item in self.test_data.index]
             )
 
-    def get_train_batch(self, batch_size=128):
-        if self.train_data_files is not None:
-            for train_data_file in self.train_data_files:
-                self.train_data = load(train_data_file).sample(frac=1).reset_index(drop=True)
-                num_batch = len(self.train_data) // batch_size
+    def get_train_batch(self, batch_size=128) -> array:
+        """Generator yield single batch of data as numpy nd array."""
+        if self._train_data_files is not None:
+            for train_data_file in self._train_data_files:
+                # Loading one file at a time for memory limitation
+                # TODO: improve by making it dynamic, optimize by available memory
+                self._train_data = load_csv(train_data_file).sample(frac=1).reset_index(drop=True)
+                num_batch = len(self._train_data) // batch_size
                 for i in range(num_batch + 1):
-                    yield self.train_data.iloc[i * batch_size : (i + 1) * batch_size].values
+                    yield self._train_data.iloc[i * batch_size : (i + 1) * batch_size].values
         # else: raise NotDefinedError('train data files are not defined')
 
-    def get_test_batch(self, num_test_items=10, random=True):
-        if self.train_data_files is not None:
+    def get_test_batch(self, num_test_items=10, by_random=True) -> array:
+        """Generator yield single batch of data as numpy nd array."""
+        if self._train_data_files is not None:
             self.prepare_test_data()
             if num_test_items == -1:
-                num_test_items = len(self.test_items)
-            if random:
-                for item in np.random.choice(self.test_items, size=num_test_items):
-                    yield self.test_data[self.test_data.index.str.contains(item)].values
+                num_test_items = len(self._test_items)
+            if by_random:
+                for item in choice(self._test_items, size=num_test_items):
+                    yield self._test_data[self._test_data.index.str.contains(item)].values
             else:
-                for item in self.test_items[:num_test_items]:
-                    yield self.test_data[self.test_data.index.str.contains(item)].values
+                for item in self._test_items[:num_test_items]:
+                    yield self._test_data[self._test_data.index.str.contains(item)].values
         # else: raise NotDefinedError('test data files are not defined')
